fa9ad047fcee6433f29739d3447fcbdddeff6910
Gökhan Sihli <gokhansihli@Mac.lan>
2026-03-08 21:24:03 +0000

    Created component structure, adding Display, ButtonPanel and Button components

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,19 @@
 import "./App.css";
+import { useState } from "react";
+import Display from "./components/Display";
+import ButtonPanel from "./components/ButtonPanel/ButtonPanel";
 
 function App() {
+  const [currentValue, setCurrentValue] = useState("0");
+
+  const handleNumberClick = (num) => {
+    setCurrentValue((prev) => (prev === "0" ? num : prev + num));
+  };
+
   return (
     <>
-      <h1>Hello World!</h1>
+      <Display currentValue={currentValue} />
+      <ButtonPanel onNumberClick={handleNumberClick} />
     </>
   );
 }
