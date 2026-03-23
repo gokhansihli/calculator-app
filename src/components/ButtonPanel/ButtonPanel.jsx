ec79623845f6f2a990a751849e9bd8e78ab5bf8c
Gökhan Sihli <gokhansihli@Mac.lan>
2026-03-23 21:00:16 +0000

    Added simple logic without style

--- src/components/ButtonPanel/ButtonPanel.jsx
+++ src/components/ButtonPanel/ButtonPanel.jsx
@@ -1,11 +1,37 @@
 import Button from "./Button/Button";
 
-export default function ButtonPanel({ onNumberClick }) {
+export default function ButtonPanel({
+  onNumberClick,
+  onOperatorClick,
+  onEqualsClick,
+  onClearClick,
+}) {
   return (
     <div>
-      <Button label="7" onClick={onNumberClick} />
-      <Button label="8" onClick={onNumberClick} />
-      <Button label="9" onClick={onNumberClick} />
+      <div>
+        <Button label="7" onClick={onNumberClick} />
+        <Button label="8" onClick={onNumberClick} />
+        <Button label="9" onClick={onNumberClick} />
+        <Button label="/" onClick={onOperatorClick} />
+      </div>
+      <div>
+        <Button label="4" onClick={onNumberClick} />
+        <Button label="5" onClick={onNumberClick} />
+        <Button label="6" onClick={onNumberClick} />
+        <Button label="X" onClick={onOperatorClick} />
+      </div>
+      <div>
+        <Button label="1" onClick={onNumberClick} />
+        <Button label="2" onClick={onNumberClick} />
+        <Button label="3" onClick={onNumberClick} />
+        <Button label="-" onClick={onOperatorClick} />
+      </div>
+      <div>
+        <Button label="0" onClick={onNumberClick} />
+        <Button label="C" onClick={onClearClick} />
+        <Button label="=" onClick={onEqualsClick} />
+        <Button label="+" onClick={onOperatorClick} />
+      </div>
     </div>
   );
 }
