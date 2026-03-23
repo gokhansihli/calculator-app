20b22e2b6cfa7fb7eb482b6550e707a6532f0b19
Gökhan Sihli <gokhansihli@Mac.lan>
2026-03-23 21:24:42 +0000

    Added css to app

--- src/components/ButtonPanel/ButtonPanel.jsx
+++ src/components/ButtonPanel/ButtonPanel.jsx
@@ -7,31 +7,26 @@ export default function ButtonPanel({
   onClearClick,
 }) {
   return (
-    <div>
-      <div>
-        <Button label="7" onClick={onNumberClick} />
-        <Button label="8" onClick={onNumberClick} />
-        <Button label="9" onClick={onNumberClick} />
-        <Button label="/" onClick={onOperatorClick} />
-      </div>
-      <div>
-        <Button label="4" onClick={onNumberClick} />
-        <Button label="5" onClick={onNumberClick} />
-        <Button label="6" onClick={onNumberClick} />
-        <Button label="X" onClick={onOperatorClick} />
-      </div>
-      <div>
-        <Button label="1" onClick={onNumberClick} />
-        <Button label="2" onClick={onNumberClick} />
-        <Button label="3" onClick={onNumberClick} />
-        <Button label="-" onClick={onOperatorClick} />
-      </div>
-      <div>
-        <Button label="0" onClick={onNumberClick} />
-        <Button label="C" onClick={onClearClick} />
-        <Button label="=" onClick={onEqualsClick} />
-        <Button label="+" onClick={onOperatorClick} />
-      </div>
+    <div className="button-panel">
+      <Button label="7" onClick={onNumberClick} />
+      <Button label="8" onClick={onNumberClick} />
+      <Button label="9" onClick={onNumberClick} />
+      <Button label="÷" onClick={onOperatorClick} />
+
+      <Button label="4" onClick={onNumberClick} />
+      <Button label="5" onClick={onNumberClick} />
+      <Button label="6" onClick={onNumberClick} />
+      <Button label="X" onClick={onOperatorClick} />
+
+      <Button label="1" onClick={onNumberClick} />
+      <Button label="2" onClick={onNumberClick} />
+      <Button label="3" onClick={onNumberClick} />
+      <Button label="-" onClick={onOperatorClick} />
+
+      <Button label="0" onClick={onNumberClick} />
+      <Button label="C" onClick={onClearClick} />
+      <Button label="=" onClick={onEqualsClick} />
+      <Button label="+" onClick={onOperatorClick} />
     </div>
   );
 }
